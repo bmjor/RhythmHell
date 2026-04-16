34664b297be85f58fd40427ba848cb42762c36ec
Brendan Tan <brendantan78@gmail.com>
2026-04-16 14:50:02 -0500

    added switch

--- Switch.c
+++ Switch.c
@@ -8,11 +8,11 @@
 #include "../inc/LaunchPad.h"
 // LaunchPad.h defines all the indices into the PINCM table
 void Switch_Init(void){
-    // write this
- 
+  IOMUX->SECCFG.PINCM[PB0INDEX] = 0x00040081; // player one button
+  IOMUX->SECCFG.PINCM[PB1INDEX] = 0x00040081; // player two button
+  IOMUX->SECCFG.PINCM[PB2INDEX] = 0x00040081; // pause button
 }
 // return current state of switches
 uint32_t Switch_In(void){
-    // write this
-  return 0; // replace this line
+  return GPIOB->DIN31_0&(0x07); // replace this line
 }
