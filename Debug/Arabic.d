ff57df12474bb9fe6d30823596afdf71d1aca46e
Brendan Tan <brendantan78@gmail.com>
2026-04-21 20:29:06 -0500

    Add .gitignore and remove tracked build files

--- Debug/Arabic.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-Arabic.o: C:/Users/brend/Downloads/MSPM0_ValvanoWare/inc/Arabic.c \
- C:/Users/brend/Downloads/MSPM0_ValvanoWare/inc/Arabic.h \
- C:/Users/brend/Downloads/MSPM0_ValvanoWare/inc/ST7735.h
-C:/Users/brend/Downloads/MSPM0_ValvanoWare/inc/Arabic.h:
-C:/Users/brend/Downloads/MSPM0_ValvanoWare/inc/ST7735.h:
