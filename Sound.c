6f08120cf86c1f69ae54db4c5ef3efd3093c686e
Brendan Tan <brendantan78@gmail.com>
2026-04-22 04:31:48 -0500

    metronome and count in

--- Sound.c
+++ Sound.c
@@ -13,13 +13,23 @@
 //#include "../inc/FIFO1.h"
 //#include "FIFO2.h"
 
-#define BEATLEN 4080
+#define BEATLEN 8996
 #define COW1LEN 10632
-#define COW2LEN 9450
+#define COW2LEN 10632
+//#define COW2LEN 9450
+#define ONELEN 3686
+#define TWOLEN 2621
+#define THREELEN 1966
+#define GOLEN 2675
 
 static uint32_t beatIndex;
 static uint32_t cow1Index;
 static uint32_t cow2Index;
+static uint32_t oneIndex;
+static uint32_t twoIndex;
+static uint32_t threeIndex;
+static uint32_t goIndex;
+
 
 void SysTick_IntArm(uint32_t period, uint32_t priority){
   SysTick->CTRL = 0x00;
@@ -41,13 +51,17 @@ void Sound_Init(void){
   beatIndex = BEATLEN;
   cow1Index = COW1LEN;
   cow2Index = COW2LEN;
+  oneIndex = ONELEN;
+  twoIndex = TWOLEN;
+  threeIndex = THREELEN;
+  goIndex = GOLEN;
 }
 void SysTick_Handler(void){ // called at 11 kHz
   uint32_t sum = 0;
   uint32_t stream1 = 0, stream2 = 0, stream3 = 0;
   uint8_t active_streams = 0;
   if (beatIndex < BEATLEN) {
-    stream1 = shoot[beatIndex]; // change later
+    stream1 = metronome[beatIndex]; // change later
     beatIndex++;
     active_streams++;
   }
@@ -57,13 +71,36 @@ void SysTick_Handler(void){ // called at 11 kHz
     active_streams++;
   }
   if (cow2Index < COW2LEN) {
-    stream3 = cow_deep[cow2Index];
+    stream3 = cow_high[cow2Index];
     cow2Index++;
     active_streams++;
   }
+  if (oneIndex < ONELEN) {
+    stream3 = one[oneIndex];
+    oneIndex++;
+    active_streams++;
+  }
+  if (twoIndex < TWOLEN) {
+    stream3 = two[twoIndex];
+    twoIndex++;
+    active_streams++;
+  }
+  if (threeIndex < THREELEN) {
+    stream3 = three[threeIndex];
+    threeIndex++;
+    active_streams++;
+  }
+  if (goIndex < GOLEN) {
+    stream3 = go[goIndex];
+    goIndex++;
+    active_streams++;
+  }
+
+
   sum = stream1 + stream2 + stream3;
   if (active_streams > 0){
     DAC_Out(sum/active_streams); // to prevent peaking
+    //DAC_Out(sum/4);
   }
   else {
     DAC_Out(0);
@@ -97,4 +134,9 @@ void Sound_Beat(void){
 // write this
   beatIndex = 0;
 }
-
+void Sound_Count(uint8_t index) {
+  if (index == 0)  threeIndex = 0;
+  else if (index == 1) twoIndex = 0;
+  else if (index == 2) oneIndex = 0;
+  else if (index == 3) goIndex = 0;
+}
